58f49d407d3bfe22784b132787b788dcf3d6146f
johan m <casa@Johan-MacBook-Air.local>
2016-12-01 21:34:12 +0100

    lite uppdateringar!

--- js/classes/player.js
+++ js/classes/player.js
@@ -1,37 +1,21 @@
-
-					// (johan says) jo, jag har problem med att rendera. har lagt till en provisorisk
-					// knapp i html som ska trigga ny player... men inget händer!!! måste lämna detta nu
-					// konferans här på skolan
-var players = [];
-
-function player() {
-   players = new Player();
+function Player() {
+	$('.headlines').append('<th class="players"></th>');
+	$('.ones').append('<td>' + 'hi' + '</td>');
+	$('.twos').append('<td>' + 'hi' + '</td>');
+	$('.threes').append('<td>' + 'hi' + '</td>');
+	$('.fours').append('<td>' + 'hi' + '</td>');
+	$('.fives').append('<td>' + 'hi' + '</td>');
+	$('.sixes').append('<td>' + 'hi' + '</td>');
+	$('.sum').append('<td>' + 'hi' + '</td>');
+	$('.bonus').append('<td>' + 'hi' + '</td>');
+	$('.one-pair').append('<td>' + 'hi' + '</td>');
+	$('.two-pairs').append('<td>' + 'hi' + '</td>');
+	$('.three-of-a-kind').append('<td>' + 'hi' + '</td>');
+	$('.four-of-a-kind').append('<td>' + 'hi' + '</td>');
+	$('.small-straight').append('<td>' + 'hi' + '</td>');
+	$('.large-straight').append('<td>' + 'hi' + '</td>');
+	$('.full-house').append('<td>' + 'hi' + '</td>');
+	$('.chance').append('<td>' + 'hi' + '</td>');
+	$('.yatzy').append('<td>' + 'hi' + '</td>');
+	$('.total').append('<td>' + 'hi' + '</td>');
 }
-
-Player.prototype.protocol = function() {
-  var player = $(
-	'<th>Player 1</th>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>' +
-	'<td class= ></td>'
-	);
-
-	player.data('player',this);
-
-  $('.table-bordered').append(player);
-};
