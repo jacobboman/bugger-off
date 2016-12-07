a10b747472cd785ce1c76af30f226d6ae60370a1
SaraJerre <sara.jerre@hotmail.com>
2016-12-07 16:41:36 +0100

    popup inkl. input-fält + player-list = FIXAT :D (dock ganska ful)

--- js/main.js
+++ js/main.js
@@ -1,52 +1,51 @@
-$(function theStart() {
+function theStart() {
 	// create a popup with all the 'choose amount of players'-buttons
 	$('body').append(
 		'<div class="popup-bg">' +
 			'<div class="dom-popup">' + 
-						'<div class="row">' +
-						  '<div class="col-lg-6">' +
-						    '<div class="input-group">'+
-							   '<input type="text" class="form-control" placeholder="Namn">' +
-								   '<span class="input-group-btn">' +
-							        	'<button class="btn btn-default add-players" type="button">Lägg till spelare</button>' +
-							      	'</span>' +
-							    '</div><!-- /input-group -->' +
-						    '<span class="name-list"></span>' +
-						  '</div><!-- /.col-lg-6 -->' +
-						'</div><!-- /.row -->' +
+				'<div class="row">' +
+				  '<div class="col-lg-6 player-input">' +
+					  	'<label for="usrnm" class="ui-hidden-accessible player-label">Spelare:</label>' +
+					  	'<input type="text" name="user" id="usrnm" placeholder="Namn">' +
+						'<input type="submit" data-inline="true" class="add-players" value="Lägg till spelare">' +
+				   '</div><!-- /.col-lg-6 -->' +
+				   '<div class="col-lg-6 player-list">' +
+				  		'<ol class="name-list"></ol>' +
+				  	'</div>' +
+				'</div><!-- /.row -->' +
 			'</div>' +
 		'</div>');
 
 
+		$('.add-players').on('click', function(event) {
+			event.preventDefault();
+			// FÖR VARJE KLICK:
+			// input-värde sparas i variabeln playerName
+		 	var playerName = $('#usrnm').val();
+		 	// name-key i protocol object = spelarens namn
+			protocol[name] = playerName;
+			// nytt Player-object skapas och värde för name-key 
+			// skickas in (m h a inparametern playerName) till protocol-objektet
+			// och sparas i variabeln newPlayer
+			var newPlayer = new Player(playerName);
+			// spelarnas namn appendas som list items i DOM
+			$('.name-list').append('<li>' + playerName + '</li>');
+			// input-fältet töms
+			$('#usrnm').val('');
+			
+			console.log(newPlayer);
+			// den nya spelaren pushas in i den globala arrayen allPlayers
+			allPlayers.push(newPlayer);
+		});
+			
+			
+			console.log(allPlayers);
+	
 
-/*	player.name = $('.form-control').val();
-	console.log(player.name);
-	player[name] = newName;
-	allPlayers.forEach(function(player) {
-		$('.name-list').append('<li>' + player.name + '</li>');
-	});
-	
-	console.log(allPlayers);
-	
-	
-
+/*	
 	// 	// remove the popup
 	// 	$('.btn-primary').on('click', function() {
 	/// 		$('.popup-bg').remove();
 	// });*/
-)};
+};
 
-$(function() {
- 	plsHelp = $('.form-control').val();
-/*	player[name] = newName;
-	$('.add-players').click(function() {
-		console.log(plsHelp);
-		new Player(plsHelp);
-		allPlayers.push(Player)
-	});
-	/*allPlayers.forEach(function(player) {
-		$('.name-list').append('<li>' + player.name + '</li>');
-	});*/
-	
-	console.log(allPlayers);
-});
